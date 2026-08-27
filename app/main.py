530555d2c0e08c7a9cfaca62188f5717d1fe9db1
Yelyzaveta Hulchenko <liza.gulchenko@gmail.com>
2026-08-27 14:24:23 +0300

    Fixed whitespaces

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
     result = []
-    
+
     if not isinstance(cat_age, int) or not isinstance(dog_age, int):
         raise TypeError("Age must be an integer.")
     if cat_age < 0 or dog_age < 0:
         raise ValueError("Age cannot be negative.")
-    
+
     if cat_age < 15:
         result.append(0)
     elif cat_age < 24:
